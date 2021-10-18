a4b9f5f0061479f1231177b02d5e72f18490bfae
SaracenOne <SaracenOne@gmail.com>
2021-10-18 11:30:59 +0100

    Replace 'linear_velocity' with 'motion_velocity'

--- extended_kinematic_body.gd
+++ extended_kinematic_body.gd
@@ -111,9 +111,9 @@ func extended_move(p_motion: Vector3, p_slide_attempts: int) -> Vector3:
 						)
 						# Do actual motion
 						# FIXME: They changed move_and_slide to have 0 arguments????
-						linear_velocity = p_motion
+						motion_velocity = p_motion
 						move_and_slide()
-						motion = linear_velocity
+						motion = motion_velocity
 						#motion = move_and_slide(
 						#	p_motion,
 						#	up,
@@ -185,9 +185,9 @@ func extended_move(p_motion: Vector3, p_slide_attempts: int) -> Vector3:
 							_step_down(dss)
 					else:
 						# FIXME: They changed move_and_slide to have 0 arguments????
-						linear_velocity = p_motion
+						motion_velocity = p_motion
 						move_and_slide()
-						motion = linear_velocity
+						motion = motion_velocity
 						#motion = move_and_slide(
 						#	p_motion, up, 0.0, p_slide_attempts, 1.0, infinite_interia
 						#)
